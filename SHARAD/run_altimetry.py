65060bce9e925dca63b87d36e169cbb8507e2f07
Gregory Ng <ngg@melt.ig.utexas.edu>
2019-04-04 18:28:07 -0500

    Moved "main" to the top

--- SHARAD/run_altimetry.py
+++ SHARAD/run_altimetry.py
@@ -28,62 +28,6 @@ import altimetry.beta5 as b5
 import gzip
 
 
-def alt_processor(inpath, outputfile, idx_start=None, idx_end=None, save_format=''):
-    try:
-        # create cmp path
-        path_root_alt = '/disk/kea/SDS/targ/xtra/SHARAD/alt/'
-        path_root_cmp = '/disk/kea/SDS/targ/xtra/SHARAD/cmp/'
-        path_root_edr = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/'
-        # Relative path to this file
-        fname = os.path.basename(inpath)
-        obn = fname[2:9] # orbit name
-        # Relative directory of this file
-        reldir = os.path.dirname(os.path.relpath(inpath, path_root_edr))
-        logging.debug("inpath: " + inpath)
-        logging.debug("reldir: " + reldir)
-        logging.debug("path_root_edr: " + path_root_edr)
-        cmp_path = os.path.join(path_root_cmp, reldir, 'ion', fname.replace('_a.dat','_s.h5') )
-        #cmp_path = path_root_cmp+path_file+'ion/'+data_file.replace('_a.dat','_s.h5')
-        label_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/science_ancillary.fmt'
-        aux_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/auxiliary.fmt'
-        science_path = inpath.replace('_a.dat','_s.dat')
-
-        if not os.path.exists(cmp_path): 
-            logging.warning(cmp_path + " does not exist")
-            return 0
-
-        logging.info("Reading " + cmp_path)
-        df = b5.beta5_altimetry(cmp_path, science_path, label_path, aux_path,
-                                idx_start=0, idx_end=None, use_spice=False, ft_avg=10,
-                                max_slope=25, noise_scale=20, fix_pri=1, fine=False)
-
-        #new_path = path_root_alt+path_file+'beta5/'
-        if save_format == 'hdf5':
-            outfile='north_pole_beta5.h5'
-            logging.info("Writing to " + outfile)
-            h5 = hdf.hdf(outfile, mode='a')
-            orbit_data = {obn: df}
-            h5.save_dict('sharad', orbit_data)
-            h5.close()
-        elif save_format == 'csv':
-            #fname1 = fname.replace('_a.dat', '.csv.gz')
-            #outfile = os.path.join(path_root_alt, reldir, 'beta5',fname1)
-            logging.info("Writing to " + outputfile)
-            if not os.path.exists( os.path.dirname(outputfile) ):
-                os.makedirs( os.path.dirname(outputfile) )
-
-            df.to_csv(outputfile)
-        elif save_format == '':
-            pass
-        else:
-            logging.warning("Unrecognized output format '{:s}'".format(save_format))
-
-    except Exception as e:
-        taskname="error"
-        for line in traceback.format_exc().split("\n"):
-            print('{:s}: {:s}'.format(taskname, line) )
-        return 1
-    
 def main():
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
         format="run_altimetry: [%(levelname)-7s] %(message)s")
@@ -156,3 +100,67 @@ def main():
             flag = result.get()
             print("Finished task {:d} of {:d}".format(i+1, len(process_list)))
         print('done')
+
+
+
+def alt_processor(inpath, outputfile, idx_start=None, idx_end=None, save_format=''):
+    try:
+        # create cmp path
+        path_root_alt = '/disk/kea/SDS/targ/xtra/SHARAD/alt/'
+        path_root_cmp = '/disk/kea/SDS/targ/xtra/SHARAD/cmp/'
+        path_root_edr = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/'
+        # Relative path to this file
+        fname = os.path.basename(inpath)
+        obn = fname[2:9] # orbit name
+        # Relative directory of this file
+        reldir = os.path.dirname(os.path.relpath(inpath, path_root_edr))
+        logging.debug("inpath: " + inpath)
+        logging.debug("reldir: " + reldir)
+        logging.debug("path_root_edr: " + path_root_edr)
+        cmp_path = os.path.join(path_root_cmp, reldir, 'ion', fname.replace('_a.dat','_s.h5') )
+        #cmp_path = path_root_cmp+path_file+'ion/'+data_file.replace('_a.dat','_s.h5')
+        label_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/science_ancillary.fmt'
+        aux_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/auxiliary.fmt'
+        science_path = inpath.replace('_a.dat','_s.dat')
+
+        if not os.path.exists(cmp_path): 
+            logging.warning(cmp_path + " does not exist")
+            return 0
+
+        logging.info("Reading " + cmp_path)
+        df = b5.beta5_altimetry(cmp_path, science_path, label_path, aux_path,
+                                idx_start=0, idx_end=None, use_spice=False, ft_avg=10,
+                                max_slope=25, noise_scale=20, fix_pri=1, fine=False)
+
+        #new_path = path_root_alt+path_file+'beta5/'
+        if save_format == 'hdf5':
+            outfile='north_pole_beta5.h5'
+            logging.info("Writing to " + outfile)
+            h5 = hdf.hdf(outfile, mode='a')
+            orbit_data = {obn: df}
+            h5.save_dict('sharad', orbit_data)
+            h5.close()
+        elif save_format == 'csv':
+            #fname1 = fname.replace('_a.dat', '.csv.gz')
+            #outfile = os.path.join(path_root_alt, reldir, 'beta5',fname1)
+            logging.info("Writing to " + outputfile)
+            if not os.path.exists( os.path.dirname(outputfile) ):
+                os.makedirs( os.path.dirname(outputfile) )
+
+            df.to_csv(outputfile)
+        elif save_format == '':
+            pass
+        else:
+            logging.warning("Unrecognized output format '{:s}'".format(save_format))
+
+    except Exception as e:
+        taskname="error"
+        for line in traceback.format_exc().split("\n"):
+            print('{:s}: {:s}'.format(taskname, line) )
+        return 1
+    
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
