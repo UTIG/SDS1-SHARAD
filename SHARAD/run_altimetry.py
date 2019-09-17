0ee1a43eb0d7674a76b157b96c9d63665824e4de
Gregor Steinbruegge <gregor@melt.ig.utexas.edu>
2019-09-17 12:52:26 -0500

    changed input

--- SHARAD/run_altimetry.py
+++ SHARAD/run_altimetry.py
@@ -34,7 +34,7 @@ def main():
     parser.add_argument('--ofmt', default='hdf5', choices=('hdf5','csv','none'),
                         help="Output file format")
 
-    parser.add_argument('-j','--jobs', type=int, default=4, help="Number of jobs (cores) to use for processing")
+    parser.add_argument('-j','--jobs', type=int, default=8, help="Number of jobs (cores) to use for processing")
     parser.add_argument('-v','--verbose', action="store_true", help="Display verbose output")
     parser.add_argument('-n','--dryrun', action="store_true", help="Dry run. Build task list but do not run")
     parser.add_argument('--tracklist', default="EDR_NorthPole_Path.txt",
@@ -70,14 +70,16 @@ def main():
             data_file = os.path.basename(path)
             outfile = os.path.join(path_out, relpath, 'beta5', data_file.replace('.dat', ext[args.ofmt]))
 
-            #if not os.path.exists(outfile):
-            process_list.append({
+            if True: #not os.path.exists(outfile):
+                process_list.append({
                     'inpath' : path, 
                     'outputfile' : outfile, 
                     'idx_start' : 0,
                     'idx_end' : None,
                     'save_format' : args.ofmt})
-            logging.debug("[{:d}] {:s}".format(i+1,str(process_list[-1])))
+                logging.debug("[{:d}] {:s}".format(i+1,str(process_list[-1])))
+            else:
+                logging.debug('file '+outfile+' already exists!')
 
     if args.maxtracks > 0 and len(process_list) > args.maxtracks:
         # Limit to first args.maxtracks tracks
@@ -116,7 +118,6 @@ def alt_processor(inpath, outputfile, idx_start=0, idx_end=None, save_format='')
         logging.debug("reldir: " + reldir)
         logging.debug("path_root_edr: " + path_root_edr)
         cmp_path = os.path.join(path_root_cmp, reldir, 'ion', fname.replace('_a.dat','_s.h5') )
-        #cmp_path = path_root_cmp+path_file+'ion/'+data_file.replace('_a.dat','_s.h5')
         label_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/science_ancillary.fmt'
         aux_path = '/disk/kea/SDS/orig/supl/xtra-pds/SHARAD/EDR/mrosh_0004/label/auxiliary.fmt'
 
@@ -124,16 +125,18 @@ def alt_processor(inpath, outputfile, idx_start=0, idx_end=None, save_format='')
 
         if not os.path.exists(cmp_path): 
             logging.warning(cmp_path + " does not exist")
-        science_path=inpath.replace('_a.dat','_s.dat')
-        if not os.path.exists(cmp_path): 
-            logging.warning(cmp_path + " does not exist")
             return 0
 
         logging.info("Reading " + cmp_path)
         result = b5.beta5_altimetry(cmp_path, science_path, label_path, aux_path,
-                                    idx_start=idx_start, idx_end=idx_end, use_spice=False, ft_avg=10,
+                                    idx_start=None, idx_end=None, use_spice=False, ft_avg=10,
                                     max_slope=25, noise_scale=20, fix_pri=1, fine=True)
 
+        
+        #import matplotlib.pyplot as plt
+        #plt.plot(result['range'])
+        #plt.show()
+ 
         if save_format == '':
             return 0
 
@@ -144,8 +147,10 @@ def alt_processor(inpath, outputfile, idx_start=0, idx_end=None, save_format='')
 
         if save_format == 'hdf5':
             orbit_data = {'orbit'+str(obn): result}
+            os.system("rm " + outputfile)
             h5 = hdf.hdf(outputfile, mode='w')
             h5.save_dict('beta5', orbit_data)
+            h5.close()
         elif save_format == 'csv':
             df.to_csv(outputfile)
         else:
