b471162602e8854be7c414be28ef40c6ab840bfb
Gregory Ng <ngg@melt.ig.utexas.edu>
2020-03-05 15:01:53 -0600

    Improvments to visualization of differences

--- MARFA/cmp_interferometry_coregistration.py
+++ MARFA/cmp_interferometry_coregistration.py
@@ -23,11 +23,11 @@ with np.load(post_coreg1) as data1, np.load(post_coreg2) as data2:
 
     cmpA1 = data1['cmpA3']
     cmpA2 = data2['cmpA3']
-    rmsa = np.sqrt(np.square(abs(cmpA1 - cmpA2)).mean(axis=1))
+    rmsa = np.sqrt(np.square(abs(cmpA1 - cmpA2)).mean(axis=0))
 
     cmpB1 = data1['cmpB3']
     cmpB2 = data2['cmpB3']
-    rmsb = np.sqrt(np.square(abs(cmpB1 - cmpB2)).mean(axis=1))
+    rmsb = np.sqrt(np.square(abs(cmpB1 - cmpB2)).mean(axis=0))
     print('RMS cmpA = {:0.3f}'.format(rmsa.mean()))
     print('RMS cmpB = {:0.3f}'.format(rmsb.mean()))
     print('size cmpA = {:s}'.format(str(cmpA1.shape)))
@@ -43,13 +43,28 @@ if bplot:
     import matplotlib.pyplot as plt
     import matplotlib.colors as col
     print("Plotting")
-    plt.subplot(211)
-    plt.plot(rmsa)
-    plt.plot(rmsb)
-    plt.legend(['RMSE cmpA','RMSE cmpB'])
+
+    fig, axes = plt.subplots(3, 1, sharex=True)
+
+    axes[0].plot(rmsa)
+    axes[0].plot(rmsb)
+    axes[0].grid()
+    axes[1].set_ylabel('Amplitude')
+    axes[0].set_title('RMS error between old and new co-registered outputs')
+    axes[0].legend(['RMSE cmpA','RMSE cmpB'])
     
-    plt.subplot(212)
-    plt.plot(shift2)
-    plt.grid()
-    plt.legend(('shift2',))
+    axes[1].plot(shift1, linewidth=1)
+    axes[1].plot(shift2, linewidth=1)
+    axes[1].grid()
+    axes[1].set_ylabel('Fast time samples')
+    axes[1].set_title('Old and new shift values')
+
+    axes[1].legend(('Old shift', 'New shift',))
+    
+    axes[2].plot(shift2 - shift1)
+    axes[2].set_title('Difference between Old Shift and New Shift')
+    axes[2].set_ylabel('Fast time samples')
+    axes[2].set_xlabel('Slow time samples')
+    axes[2].grid()
+    #plt.legend(('dshift',))
     plt.show()
