6cbf693742b3cadfaa8883c4e7fe6e628eeee9f7
Gregory Ng <ngg@melt.ig.utexas.edu>
2020-03-12 00:53:48 -0500

    Basic regression tests for peakint.

--- xlib/clutter/peakint.py
+++ xlib/clutter/peakint.py
@@ -2,7 +2,11 @@
 
 """
 Peak interpolation
+
+# TODO: Unify qint3 and qint5 methods to have a more uniform prototype that we can swap
+
 """
+import numpy as np
 
 # From Julius Smith Spectral Audio Signal Processing
 # peak finding algorithm (qint.m)
@@ -56,7 +60,8 @@ def qint5(y):
     vmax_val = a + b*vmax_pos + c * vmax_pos*vmax_pos
     # Should always be within one sample of the max.
     # assert -1 < vmax_pos < 1
-    return (vmax_pos, vmax_val, None)
+    # TODO: verify half-curvature
+    return (vmax_pos, vmax_val, c)
 
 def qint(y, maxpos):
     if maxpos >= 2 and maxpos <= len(y) - 3:
@@ -68,9 +73,85 @@ def qint(y, maxpos):
         return (0, y[maxpos], None)
 
 
+def test3():
+    eps = 1e-6
+    # TODO: test boundaries
+    x = np.arange(-1, 13, 1)
+    print("start test3()")
+    for a in np.arange(-10, -0.1, 0.1):
+        for h in np.arange(0.0, 10.0, 0.1):
+            for k in np.arange(-10, 10.0, 2):
+                # known vertex h, k
+                data = a * ((x - h) ** 2) + k
+                x1 = np.argmax(data)
+                p, y, a = qint3(data[x1 - 1], data[x1], data[x1 + 1])
+                p1  = x[x1] + p
+                try:
+                    assert abs(p1 - h) < eps
+                    assert abs(y - k) < eps
+                except AssertionError: #pragma: no cover
+                    print("h,k=({:f},{:f}) p1, y, a = {:f} {:f} {:f}".format(h, k, p1, y, a))
+                    raise
+    print("end test3()")
+def test5():
+    """ test 5-point method """
+    # TODO: test boundaries
+    eps = 1e-6
+    x = np.arange(-2, 15, 1)
+    print("start test5()")
+    for a in np.arange(-10, -0.1, 0.1):
+        for h in np.arange(0.0, 10.0, 0.1):
+            for k in np.arange(-10, 10.0, 2):
+                # known vertex h, k
+                data = a * ((x - h) ** 2) + k
+                x1 = np.argmax(data)
+                p, y, a = qint5(data[x1 - 2:x1 + 3])
+                p1  = x[x1] + p
+                try:
+                    assert abs(p1 - h) < eps
+                    assert abs(y - k) < eps
+                except AssertionError: #pragma: no cover
+                    print("h,k=({:f},{:f}) p1, y, a = {:f} {:f} {:f}".format(h, k, p1, y, a))
+                    raise
+    print("end test5()")
+
+
+def test_qint():
+    eps = 1e-6
+
+    testdata = [
+        [0.5, 1.0, 2.0, 1.0, 0.5], 
+        [0.5, 2.0, 1.0, 1.0, 0.5],
+        # This fails equivalence if equal max values
+        #[2.0, 2.0, 1.0, 1.0, 0.5],
+        [3.0, 2.0, 1.0, 1.0, 0.5],
+    ]
+
+    for data in testdata:
+        npdata = np.array(data)
+        x1 = np.argmax(npdata)
+        p1, y1, a1 = qint(npdata, x1)
+
+        data.reverse()
+        npdata = np.array(data)
+        x2 = np.argmax(npdata)
+        p2, y2, a2 = qint(npdata, x2)
+
+        try:
+            assert abs(p1 + p2) < eps # p1 == -p2
+            assert abs(y1 - y2) < eps # y1 == y2
+            assert a1 is None or abs(a1 - a2) < eps # a1 == a2
+        except AssertionError: #pragma: no cover
+            print("data={:s}; x1={:d} x2={:d} p1={:f} p2={:f}, y1={:f} y2={:f} "
+                  "a1={:s} a2={:s}".format(str(data), x1, x2, p1, p2, y1, y2, str(a1), str(a2)))
+            raise
+        # TODO: check result
+
 def main():
-    # TODO: testing functions for peak interpolation
-    pass
+    """ testing functions for peak interpolation """
+    test_qint()
+    test3()
+    test5()
 
 if __name__=="__main__":
     main()
